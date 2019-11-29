282a5541411792eb627aa231251c93c295e93f51
skyreborn <790674452@qq.com>
2019-11-29 13:46:38 +0800

    hui后台模板html文件整合tp5框架，css与js文件路径调整

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+    	return $this->fetch();
+    }
+
+    public function welcome()
+    {
+    	return $this->fetch();
+    }
+}
