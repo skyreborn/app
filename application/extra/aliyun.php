00a07ef26bcd1042593f96a51d98b505b4782407
skyreborn <790674452@qq.com>
2020-01-12 22:32:34 +0800

    继续安全

--- application/extra/aliyun.php
+++ application/extra/aliyun.php
@@ -5,12 +5,12 @@
  * @Autor: sky 1127820180@qq.com
  * @Date: 2020-01-06 14:35:33
  * @LastEditors  : sky 1127820180@qq.com
- * @LastEditTime : 2020-01-12 22:09:38
+ * @LastEditTime : 2020-01-12 22:29:16
  */
 return [
-    'SignName' => 'sky新闻app',
-    'TemplateCode' => 'SMS_181867799',
-    'accessKeyId' => 'LTAI4FtsTRz756ZuassQ5RJ7',
-    'accessSecret' => 'y3l0dzbO7vaPHVrTlg9lgMWIS6pWq3',
+    'SignName' => '',
+    'TemplateCode' => '',
+    'accessKeyId' => '',
+    'accessSecret' => '',
     'identify_time' => 120,
  ];
